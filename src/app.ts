de64089880a49a0bec37f19fcb3ff94e722aa0ce
Rafael Mendes <mendesrafa.c@gmail.com>
2020-12-15 22:34:12 -0300

    adding babel config for relative paths

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default db;
